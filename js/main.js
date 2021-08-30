2a36447e6641b60b0002eb6a9cb0e70feb7250e6
Himanshu Gulati <64896622+iamhimanshugulati@users.noreply.github.com>
2021-08-30 20:56:44 +0530

    update_json

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Fetching JSON Format file
-fetch("https://run.mocky.io/v3/bf175661-5e9f-4112-8580-d587759ff72e")
+fetch("/json/myntra_api.json")
 
     // If fetched file Successfully, parsing in JSON format in console
     .then((response) => response.json())
